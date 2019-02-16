85b9d26414341a0de5a82d92bd198bae36e8b31b
rsquaredin <rsquaredin@gmail.com>
2019-02-16 13:19:34 +0530

    reknit

---
+++ data-wrangling/regex/intro.R
@@ -0,0 +1,10 @@
+library(stringr)
+
+# lesson 1
+# \d can be used in place of any digit from 0 to 9
+# \. can match any single character (wildcard)
+# match specific characters by defining them inside []
+# exclude specific characters using hat [^]
+# a{3} will match the a character exactly 3 times
+# a{1,3} will match the a character no more than 3 times, but no less than once
+
